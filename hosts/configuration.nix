c15bcc9f9ecfd19a42a4e2e875eb159b46bde701
racoot <sukhmanghum66778899@gmail.com>
2024-04-22 19:17:50 -0400

    updated npm-android shell

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -21,20 +21,23 @@
 #       └─ ./theming
 #           └─ default.nix
 #
-
-{ pkgs, unstable, inputs, vars, ... }:
-
-let
-  terminal = pkgs.${vars.terminal};
-in
 {
-  imports = (import ../modules/desktops ++
-    import ../modules/editors ++
-    import ../modules/hardware ++
-    import ../modules/programs ++
-    import ../modules/services ++
-    import ../modules/shell ++
-    import ../modules/theming);
+  pkgs,
+  unstable,
+  inputs,
+  vars,
+  ...
+}: let
+  terminal = pkgs.${vars.terminal};
+in {
+  imports =
+    import ../modules/desktops
+    ++ import ../modules/editors
+    ++ import ../modules/hardware
+    ++ import ../modules/programs
+    ++ import ../modules/services
+    ++ import ../modules/shell
+    ++ import ../modules/theming;
 
   boot = {
     tmp = {
@@ -46,7 +49,7 @@ in
 
   users.users.${vars.user} = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "video" "audio" "camera" "networkmanager" "lp" "scanner" ];
+    extraGroups = ["wheel" "video" "audio" "camera" "networkmanager" "lp" "scanner"];
   };
 
   time.timeZone = "America/Toronto";
@@ -91,62 +94,63 @@ in
       EDITOR = "${vars.editor}";
       VISUAL = "${vars.editor}";
     };
-    systemPackages = with pkgs; [
-      # Terminal
-      terminal # Terminal Emulator
-      btop # Resource Manager
-      coreutils # GNU Utilities
-      git # Version Control
-      killall # Process Killer
-      lshw # Hardware Config
-      nano # Text Editor
-      nodejs # Javascript Runtime
-      nodePackages.pnpm # Package Manager
-      nix-tree # Browse Nix Store
-      pciutils # Manage PCI
-      ranger # File Manager
-      smartmontools # Disk Health
-      tldr # Helper
-      usbutils # Manage USB
-      wget # Retriever
-      xdg-utils # Environment integration
+    systemPackages = with pkgs;
+      [
+        # Terminal
+        terminal # Terminal Emulator
+        btop # Resource Manager
+        coreutils # GNU Utilities
+        git # Version Control
+        killall # Process Killer
+        lshw # Hardware Config
+        nano # Text Editor
+        nodejs # Javascript Runtime
+        nodePackages.pnpm # Package Manager
+        nix-tree # Browse Nix Store
+        pciutils # Manage PCI
+        ranger # File Manager
+        smartmontools # Disk Health
+        tldr # Helper
+        usbutils # Manage USB
+        wget # Retriever
+        xdg-utils # Environment integration
 
-      # Video/Audio
-      alsa-utils # Audio Control
-      feh # Image Viewer
-      image-roll # Image Viewer
-      linux-firmware # Proprietary Hardware Blob
-      mpv # Media Player
-      pavucontrol # Audio Control
-      pipewire # Audio Server/Control
-      pulseaudio # Audio Server/Control
-      qpwgraph # Pipewire Graph Manager
-      vlc # Media Player
+        # Video/Audio
+        alsa-utils # Audio Control
+        feh # Image Viewer
+        image-roll # Image Viewer
+        linux-firmware # Proprietary Hardware Blob
+        mpv # Media Player
+        pavucontrol # Audio Control
+        pipewire # Audio Server/Control
+        pulseaudio # Audio Server/Control
+        qpwgraph # Pipewire Graph Manager
+        vlc # Media Player
 
-      # Apps
-      appimage-run # Runs AppImages on NixOS
-      firefox # Browser
-      google-chrome # Browser
-      remmina # XRDP & VNC Client
+        # Apps
+        appimage-run # Runs AppImages on NixOS
+        firefox # Browser
+        google-chrome # Browser
+        remmina # XRDP & VNC Client
 
-      # File Management
-      gnome.file-roller # Archive Manager
-      pcmanfm # File Browser
-      p7zip # Zip Encryption
-      rsync # Syncer - $ rsync -r dir1/ dir2/
-      unzip # Zip Files
-      unrar # Rar Files
-      wpsoffice # Office
-      zip # Zip
+        # File Management
+        gnome.file-roller # Archive Manager
+        pcmanfm # File Browser
+        p7zip # Zip Encryption
+        rsync # Syncer - $ rsync -r dir1/ dir2/
+        unzip # Zip Files
+        unrar # Rar Files
+        wpsoffice # Office
+        zip # Zip
 
-      # Other Packages Found @
-      # - ./<host>/default.nix
-      # - ../modules
-    ] ++
-    (with unstable; [
-      # Apps
-      # firefox # Browser
-    ]);
+        # Other Packages Found @
+        # - ./<host>/default.nix
+        # - ../modules
+      ]
+      ++ (with unstable; [
+        # Apps
+        # firefox # Browser
+      ]);
   };
 
   programs = {
@@ -181,6 +185,7 @@ in
   nix = {
     settings = {
       auto-optimise-store = true;
+      warn-dirty = false;
     };
     gc = {
       automatic = true;
@@ -217,21 +222,21 @@ in
       mimeApps = {
         enable = true;
         defaultApplications = {
-          "image/jpeg" = [ "image-roll.desktop" "feh.desktop" ];
-          "image/png" = [ "image-roll.desktop" "feh.desktop" ];
+          "image/jpeg" = ["image-roll.desktop" "feh.desktop"];
+          "image/png" = ["image-roll.desktop" "feh.desktop"];
           "text/plain" = "nvim.desktop";
           "text/html" = "nvim.desktop";
           "text/csv" = "nvim.desktop";
-          "application/pdf" = [ "wps-office-pdf.desktop" "firefox.desktop" "google-chrome.desktop" ];
+          "application/pdf" = ["wps-office-pdf.desktop" "firefox.desktop" "google-chrome.desktop"];
           "application/zip" = "org.gnome.FileRoller.desktop";
           "application/x-tar" = "org.gnome.FileRoller.desktop";
           "application/x-bzip2" = "org.gnome.FileRoller.desktop";
           "application/x-gzip" = "org.gnome.FileRoller.desktop";
-          "x-scheme-handler/http" = [ "firefox.desktop" "google-chrome.desktop" ];
-          "x-scheme-handler/https" = [ "firefox.desktop" "google-chrome.desktop" ];
-          "x-scheme-handler/about" = [ "firefox.desktop" "google-chrome.desktop" ];
-          "x-scheme-handler/unknown" = [ "firefox.desktop" "google-chrome.desktop" ];
-          "x-scheme-handler/mailto" = [ "gmail.desktop" ];
+          "x-scheme-handler/http" = ["firefox.desktop" "google-chrome.desktop"];
+          "x-scheme-handler/https" = ["firefox.desktop" "google-chrome.desktop"];
+          "x-scheme-handler/about" = ["firefox.desktop" "google-chrome.desktop"];
+          "x-scheme-handler/unknown" = ["firefox.desktop" "google-chrome.desktop"];
+          "x-scheme-handler/mailto" = ["gmail.desktop"];
           "audio/mp3" = "mpv.desktop";
           "audio/x-matroska" = "mpv.desktop";
           "video/webm" = "mpv.desktop";
@@ -243,12 +248,12 @@ in
       desktopEntries.image-roll = {
         name = "image-roll";
         exec = "${pkgs.image-roll}/bin/image-roll %F";
-        mimeType = [ "image/*" ];
+        mimeType = ["image/*"];
       };
       desktopEntries.gmail = {
         name = "Gmail";
         exec = ''xdg-open "https://mail.google.com/mail/?view=cm&fs=1&to=%u"'';
-        mimeType = [ "x-scheme-handler/mailto" ];
+        mimeType = ["x-scheme-handler/mailto"];
       };
     };
   };
