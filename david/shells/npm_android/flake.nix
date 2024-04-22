5bf3cbb937c321fe1c570e10498c68c5dcdd4721
racoot <sukhmanghum66778899@gmail.com>
2024-04-22 17:07:19 -0400

    updated npm-android shell

--- david/shells/npm_android/flake.nix
+++ david/shells/npm_android/flake.nix
@@ -22,7 +22,7 @@
         androidComposition = androidEnv.composeAndroidPackages {
           cmdLineToolsVersion = "8.0"; # emulator related: newer versions are not only compatible with avdmanager
           platformToolsVersion = "34.0.4";
-          buildToolsVersions = ["30.0.3" "33.0.2" "34.0.0"];
+          buildToolsVersions = ["33.0.0" "30.0.3" "33.0.2" "34.0.0"];
           platformVersions = ["28" "31" "32" "33" "34"];
           abiVersions = ["x86_64"]; # emulator related: on an ARM machine, replace "x86_64" with
           # either "armeabi-v7a" or "arm64-v8a", depending on the architecture of your workstation.
