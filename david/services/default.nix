38c98b75254c6a04b35d32c18efc090e51dcbeee
racoot <sukhmanghum66778899@gmail.com>
2024-04-27 04:00:36 -0400

    updated firewall

--- david/services/default.nix
+++ david/services/default.nix
@@ -4,7 +4,7 @@
   ./polkit_service.nix
   ./psd.nix
   #./vastai_certificates.nix
-  ./firewall.nix # comment this line as a workaround when using virt-manager because libvirt uses iptables and i am using nftables so they are in conflict (TODO).
+  #  ./firewall.nix # comment this line as a workaround when using virt-manager because libvirt uses iptables and i am using nftables so they are in conflict (TODO).
   ./pam.nix
   ./wifi.nix
   # ./disable_nvidia.nix
--- david/services/firewall.nix
+++ david/services/firewall.nix
@@ -1,36 +1,30 @@
 #
-# nftables configuration 
-# 
+# nftables configuration
+#
 # use the following command to get rev, hash, and other details about the repository
 # nix-prefetch-git https://github.com/<username>/<repo>
 #
-
-{ pkgs, vars, ... }:
-
-let 
+{pkgs, ...}: let
   # configurationPath = pkgs.fetchurl {
   #   url = "https://raw.githubusercontent.com/sukhmancs/nixos-config/main/modules/services/firewall/nftables.conf";
   #   sha256 = "073k9rnpk380vygy4i7dw0ryfpsb7hwmis0w19y7wns6bhbsi2pa";
   # };
-  
-  rev = "9ce0685c54430defac174a90febf985428e8116e";  
-  hash = "sha256-Aqd0a6Yx8FZdF5rvT12XhxPtNPau7qxZt7+qsKpQILE=";
+  rev = "acf3184da576324fa6e9b7d85d1bf909feb39d45";
+  hash = "sha256-/hBV4r4761fg208o93vE4pJAhlXpNCzeI3TFpIDt6Dc=";
   configurationPath = pkgs.fetchFromGitHub {
     owner = "sukhmancs";
-    repo = "nixos-config";    
+    repo = "nixos-config-main";
     rev = rev;
     hash = hash;
   };
-  
-  filePath = "${configurationPath}/modules/services/firewall/nftables.conf";
-in 
-{
+
+  filePath = "${configurationPath}/david/services/firewall/nftables.conf";
+in {
   networking = {
     nftables = {
-      enable = true;  
+      enable = true;
       # read content from the configurationPath
-      ruleset = builtins.readFile filePath;        
+      ruleset = builtins.readFile filePath;
     };
   };
 }
-
