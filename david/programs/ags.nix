aa3e85bf2e1a7c883da5a93164db0662c3486e24
racoot <sukhmanghum66778899@gmail.com>
2024-04-28 03:15:29 -0400

    added ags

--- david/programs/ags.nix
+++ david/programs/ags.nix
@@ -1,22 +1,25 @@
 {
   inputs,
   pkgs,
+  vars,
   ...
 }: {
-  # add the home manager module
-  imports = [inputs.ags.homeManagerModules.default];
+  home-manager.users.${vars.user} = {
+    # add the home manager module
+    imports = [inputs.ags.homeManagerModules.default];
 
-  programs.ags = {
-    enable = true;
+    programs.ags = {
+      enable = true;
 
-    # null or path, leave as null if you don't want hm to manage the config
-    configDir = ../ags;
+      # null or path, leave as null if you don't want hm to manage the config
+      configDir = ../ags;
 
-    # additional packages to add to gjs's runtime
-    extraPackages = with pkgs; [
-      gtksourceview
-      webkitgtk
-      accountsservice
-    ];
+      # additional packages to add to gjs's runtime
+      extraPackages = with pkgs; [
+        gtksourceview
+        webkitgtk
+        accountsservice
+      ];
+    };
   };
 }
