734610e301f2df6ce5bed9fda5dca8823e071648
racoot <sukhmanghum66778899@gmail.com>
2024-04-28 15:08:39 -0400

    disabled a lot of services

--- david/programs/default.nix
+++ david/programs/default.nix
@@ -2,12 +2,12 @@
   ./firefox.nix
   ./chromium_policies.nix
   ./vscode.nix
-  ./firejail.nix
+  # ./firejail.nix
   # ./nix-ld.nix
   ./packages.nix
   ./nix_index.nix
   # ./nix_alien.nix
-  ./pcloud.nix
+  # ./pcloud.nix
   ./gnupg.nix
   ./pass.nix
   ./rofi.nix
