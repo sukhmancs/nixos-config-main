613d13704e2ce026885c0d6b0e3cb5a22a48fc47
racoot <sukhmanghum66778899@gmail.com>
2024-05-11 20:18:48 -0400

    added kdeconnect

--- modules/programs/kdeconnect.nix
+++ modules/programs/kdeconnect.nix
@@ -1,20 +1,32 @@
 {
   lib,
   config,
+  host,
   ...
-}: let
-  inherit (lib) mkEnableOption;
-  cfg = config.modules.programs.kdeconnect;
+}:
+with host; let
+  inherit (lib) mkOption types;
+  cfg = config.modules.programs;
 in {
   options.modules.programs = {
-    kdeconnect = {
-      enable = mkEnableOption "Enable kdeconnect";
-      indicator.enable = mkEnableOption "Enable kdeconnect indicator";
+    kdeconnect = mkOption {
+      type = types.bool;
+      default = hostName != "server" && hostName != "vm";
+      description = ''
+        Enable kdeconnect for laptop and desktop
+      '';
+    };
+    indicator = mkOption {
+      type = types.bool;
+      default = hostName != "server" && hostName != "vm";
+      description = ''
+        Enable indicator for kdeconnect
+      '';
     };
   };
 
-  services.kdeconnect = lib.mkIf cfg.enable {
+  services.kdeconnect = lib.mkIf cfg.kdeconnect {
     enable = true;
-    indicator = cfg.indicator.enable;
+    indicator = cfg.indicator;
   };
 }
